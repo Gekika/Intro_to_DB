eff53769602b6d93193901006fd3b32f2c146e98
gekika <gkkaranja2@gmail.com>
2025-08-30 18:16:08 +0300

    Creating a DataBase schema for my Book Store

---
+++ alx_book_store.sql
@@ -0,0 +1,15 @@
+CREATE TABLE Books(
+	book_id (Primary Key)
+	title VARCHAR(130)
+	author_id (Foreign Key referencing Authors table)
+	price DOUBLE
+	publication_date DATE), 
+Authors(
+	author_id (Primary Key)
+	author_name VARCHAR(215)),
+Customers(
+	customer_id (Primary Key)
+	customer_name VARCHAR(215)
+	email VARCHAR(215)
+	address TEXT),
+
