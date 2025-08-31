b2c9e63a93b5f3e26cb9a87ec8cbff5d9f6dc0cc
gekika <gkkaranja2@gmail.com>
2025-08-31 23:56:04 +0300

    Creating the tables

--- task_2.sql
+++ task_2.sql
@@ -14,13 +14,13 @@ DROP TABLE IF EXISTS authors;
 DROP TABLE IF EXISTS customers;
 
 -- CREATE AUTHORS TABLE
-CREATE TABLE authors (
+CREATE TABLE Authors (
     author_id INT AUTO_INCREMENT PRIMARY KEY,
     author_name VARCHAR(215) NOT NULL
 );
 
 -- CREATE BOOKS TABLE
-CREATE TABLE books (
+CREATE TABLE Books (
     book_id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(130) NOT NULL,
     author_id INT NOT NULL,
@@ -30,7 +30,7 @@ CREATE TABLE books (
 );
 
 -- CREATE CUSTOMERS TABLE
-CREATE TABLE customers (
+CREATE TABLE Customers (
     customer_id INT AUTO_INCREMENT PRIMARY KEY,
     customer_name VARCHAR(215) NOT NULL,
     email VARCHAR(215) NOT NULL,
@@ -38,7 +38,7 @@ CREATE TABLE customers (
 );
 
 -- CREATE ORDERS TABLE
-CREATE TABLE orders (
+CREATE TABLE Orders (
     order_id INT AUTO_INCREMENT PRIMARY KEY,
     customer_id INT NOT NULL,
     order_date DATE,
@@ -46,7 +46,7 @@ CREATE TABLE orders (
 );
 
 -- CREATE ORDER_DETAILS TABLE
-CREATE TABLE order_details (
+CREATE TABLE Order_details (
     orderdetailid INT AUTO_INCREMENT PRIMARY KEY,
     order_id INT NOT NULL,
     book_id INT NOT NULL,
